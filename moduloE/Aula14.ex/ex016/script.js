a1cf0da6a24a8b32f3657df517109cd8dc1e1e37
LeoSergio <leandrooos222@gmail.com>
2023-11-28 18:23:58 -0300

    commit

--- moduloE/Aula14.ex/ex016/script.js
+++ moduloE/Aula14.ex/ex016/script.js
@@ -1,4 +1,43 @@
+function contar() {
+    var inicio = document.getElementById('start').value;
+    var v1 = Number(inicio);
+
+    var fim = document.querySelector('input#end').value;
+    var v2 = Number(fim);
+
+    var passo = document.getElementById('pass').value;
+    var v3 = Number(passo);
+
+    var msg = document.getElementById('msg'); // Adicionando a referência ao elemento onde o resultado será exibido
+    var res = v1;
+
+    for (var ini = res; res <= v2; res += v3) {
+        if(res == 0){
+            alert('[ERRO] o Passo não pode ser 0');
+        }
+        msg.innerHTML = `Sua contagem é ${res}`;
+    }
+}
+
 /*
+
+function contar(){
+    var inicio = document.getElementById('start');
+    var v1 = inicio.value;
+
+    var fim = document.querySelector('input#end');
+    var v2 = fim.value;
+
+    var passo = document.getElementById('pass');
+    var v3 = passo.value;
+
+    var ini = v1;
+    var res = v1 + v3;
+    
+    for(var ini = res; res <= v2; res += v3){
+        msg.innerHTML = `Sua contagem é ${res}`;
+    } 
+}
 esse Programa vai pegar o valor do inicio e fim e vai mostrar a diferença entre ele de acordo com o passo. 
 Ex: Inicio:5
     fim:12:
@@ -7,20 +46,3 @@ Ex: Inicio:5
 obs:Se não tiver inicio, mostrar mensagem impossivel contar.
     se o passo for 0 exibir um alert com 'Passo invalido, considerando passo1 e o passa vai ser considerado 1'
 */
-
-function contar(){
-    var inicio = document.getElementById('start');
-    var v1 = inicio;
-
-    var fim = document.querySelector('input#end');
-    var v2 = fim;
-
-    var passo = document.getElementById('pass');
-    var v3 = passo;
-
-    var res = v1;//tem que ser menor q o fim;
-
-    for(var ini = res; res <= fim; v3){
-        msg.innerHTML = `Essa é a Contagem ${res}`;
-    }
-}
