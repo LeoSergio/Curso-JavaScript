f63526d7713068b1e14fb312648f5cfcf203de63
LeoSergio <leandrooos222@gmail.com>
2023-11-30 17:20:15 -0300

    commit

--- moduloF/Exercicios4/script.js
+++ moduloF/Exercicios4/script.js
@@ -22,8 +22,38 @@ function inLista(n,l){
 
 function adicionar(){
     if(isNumero(num.value) && !inLista(num.value,valores)){
-        
+        valores.push(Number(num.value));
+        let item = document.createElement('option');
+        item.text = `Valor ${num.value} Adicionado.`
+        lista.appendChild(item);
+        res.innerHTML = '';
     }else{
-        alert('Valor invalido')
+        alert('Valor invalido ou já encontrado na lista');
+    }
+    num.value = '';
+    num.focus();
+}
+function finalizar(){
+    if(valores.length == 0){
+        alert(`Adicione um número para poder finalizar`)
+    }else{
+        let tot = valores.length;
+        let maior = valores[0];
+        let menor = valores[0];
+
+    for( let pos in valores){
+        soma+= valores[pos];
+        if(valores[pos] > maior)
+            maior = valores[pos];
+        if(valores[pos] < menor)
+            menor = valores[pos];
+    }
+    media = soma / tot;
+    res.innerHTML = '';
+    res.innerHTML += `<p>Ao todo, temos ${tot} números cadastrados.</p>`;
+    res.innerHTML += `<p>O maior número cadastrado foi ${maior}</p>`;
+    res.innerHTML += `<p>O menor número cadastrado foi ${menor}</p>`;
+    res.innerHTML = `<p>Somando todos os números temos ${soma}</p>`;
+    res.innerHTML = `<p>A media dos valores é ${media}</p>`;
     }
 }
