11bee546c79fedbf99fb78fb2a0d77edf39a5875
LeoSergio <leandrooos222@gmail.com>
2023-12-06 16:03:42 -0300

    commit

--- Ex-Pratica/Condições Básicas/ex17-21/script18.js
+++ Ex-Pratica/Condições Básicas/ex17-21/script18.js
@@ -6,13 +6,13 @@ function voting(){
     //16 opcional;
      //18 - 59 obrigatorio;
     // 60 Opcional;
-    
+
     let vot = document.getElementById('vot');
     let v = Number(vot.value);
 
     if( v >= 18 && v <= 59){
         msg.innerHTML = `Voto obrigatório`;
-    }else if((v < 18 && v >=16) || v >=60) {
+    }else if((v >=16 && v < 18  ) || v >=60) {
         msg.innerHTML = `Voto Opcional`;
     }else{
         msg.innerHTML = `Não vota`
