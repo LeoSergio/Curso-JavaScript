dfe6dec6198a33a8ed3fe1da85474ce5fa9a43a4
LeoSergio <leandrooos222@gmail.com>
2023-12-18 17:25:03 -0300

    commit

---
+++ Ex-Pratica/Estrutura de Repetições/script44.js
@@ -0,0 +1,17 @@
+/*
+Crie um algoritmo que leia o valor inicial da contagem, o valor final e o
+incremento, mostrando em seguida todos os valores no intervalo:
+Ex: Digite o primeiro Valor: 3
+Digite o último Valor: 10
+Digite o incremento: 2
+Contagem: 3 5 7 9 Acabou!
+*/
+function loop(){
+    let num1 = document.getElementById('num1');
+    let inicio = Number(num1.value);
+    let num2 = document.querySelector('#num2');
+    let final = Number(num2.value);
+    let incre = document.querySelector('#invcre');
+
+    
+}
