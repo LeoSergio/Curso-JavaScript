882b6340b9389ae491fb86bfeb73871f7f13fd88
LeoSergio <leandrooos222@gmail.com>
2023-12-28 18:00:15 -0300

    commit

--- Ex-Pratica/Estrutura de Repetições/script46.js
+++ Ex-Pratica/Estrutura de Repetições/script46.js
@@ -4,8 +4,7 @@ Crie um programa que calcule e mostre na tela o resultado da soma entre 6 +
 */
 
 function loop(){
-    
-    
+     
 for(let i=6; i<=100; i+=2){
     let ct = i;
     res.innerHTML += `${ct} ,`;
